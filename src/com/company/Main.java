29f9fafd456ac674630a9ae6de3808db65b25780
nanaishere <naiavasilyeva@gmail.com>
2021-08-11 14:53:11 +0900

    Added files

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20;
+        int ticketprice = 200;
+        int totalmiles = ticketprice/mile;
+        System.out.println (totalmiles);
+    }
+}
+
